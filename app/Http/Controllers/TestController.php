a0bd6d8cd9c780024cb1c3467951ba57e766c153
Your Name <you@example.com>
2018-06-26 17:43:22 +0800

    feat: 添加 jwt 认证
    主要修改内容如下
    	- 添加 jwt 认证扩展
    	- 配置 jwt
    	- 添加 jwt 测试代码
    	- 测试无误, 可以正常使用
    
    save

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+
 
 class TestController extends Controller
 {
@@ -13,6 +15,35 @@ class TestController extends Controller
             'message' => 'success',
             'data' => [
                 'hello' =>'hello world'
+            ]
+        ]);
+    }
+
+    public function login()
+    {
+        $User = new User();
+        $User->name = str_random(10);
+        $User->password = password_hash('1111111', 1);
+        $User->email = $User->name.'@test.com';
+        $User->save();
+        $token = auth()->login($User);
+        return response()->json([
+            'code' => 0,
+            'message' => 'token generate success',
+            'data' => [
+                'token' => $token
+            ]
+        ]);
+    }
+
+    public function info()
+    {
+        $User = auth()->user();
+        return response()->json([
+            'code' => 0,
+            'message' => 'get user_info success',
+            'data' => [
+                'test' => $User
             ]
         ]);
     }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,28 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function($api){
     $api->get('hello', 'App\Http\Controllers\TestController@hello');
 });
+
+/**
+ * 测试 jwt
+ */
+
+$api->version('v1', function ($api)
+{
+    /**
+     * 生成令牌
+     * @username string 用户名
+     * @Password string 用户密码
+     */
+    $api->post('login', 'App\Http\Controllers\TestController@login');
+
+    $api->group([
+        'middleware'=>'jwt.auth'
+    ], function ($api)
+    {
+        /**
+         * 查看令牌信息
+         * @token string 令牌
+         */
+        $api->get('test','App\Http\Controllers\TestController@info');
+    });
+});
