fb880a85d53b1ca98abf9ecf5d17878fdb91de1c
jianru <864345280@qq.com>
2018-08-01 18:09:18 +0800

    refactor:整理完善骑手用户端
    整理完善骑手端用户接口

--- app/Http/Controllers/Rider/V1/UserController.php
+++ app/Http/Controllers/Rider/V1/UserController.php
@@ -11,24 +11,20 @@ namespace App\Http\Controllers\Rider\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Rider\AccountSetRequest;
 use App\Http\Requests\Rider\CertificateRequest;
-use App\Http\Requests\Rider\PasswordResetRequest;
 use App\Http\Requests\Rider\PhoneResetRequest;
-use App\Http\Requests\Rider\SuggestionPostRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Validator;
-use Tymon\JWTAuth\Facades\JWTAuth;
 
 /**
- * @Resource("User")
+ * @Resource("骑手端用户API")
  */
 class UserController extends Controller {
 
     /**
      * 骑手基本信息
-     * @Get("/api/rider/basic_info")
+     * @Get("/api/rider/basicinfo")
      * @Version({"v1"})
-     * @Request({})
+     * @Response(200, body={"code":0, "message": "","data": ""})
      */
     public function getBasicInfo (Request $request) {
         $data = [
@@ -47,6 +43,7 @@ class UserController extends Controller {
      * @Post("/api/rider/phone/reset")
      * @Version({"v1"})
      * @Request({"old_mobile":"旧手机号","new_mobile":"新手机号","verify_code":"验证码"})
+     * @Response(200, body={"code":0, "message": "","data": ""})
      */
     public function resetPhone (PhoneResetRequest $request) {
         $old_mobile = '15611111111';
@@ -63,9 +60,9 @@ class UserController extends Controller {
 
     /**
      * 骑手本月战绩
-     * @Get("/api/rider/month_score")
+     * @Get("/api/rider/monthscore")
      * @Version({"v1"})
-     * @Request({})
+     * @Response(200, body={"code":0, "message": "","data": ""})
      */
     public function getMonthScore () {
         $data = [
@@ -82,8 +79,10 @@ class UserController extends Controller {
      * 骑手实名认证
      * @Post("/api/rider/certificate")
      * @Version({"v1"})
-     * @Request({"name":"ling","mobile":"15611111111","email":"aa@qq.com","address":"东圃米研","safe_code":"222xx","driver_permit":"1.jpg","address_permit":"2.jpg","car_insurance":"3.jpg",
+     * @Request({"name":"ling","mobile":"15611111111","email":"aa@qq.com","address":"东圃米研","safe_code":"222xx",
+     *          "driver_permit":"1.jpg","address_permit":"2.jpg","car_insurance":"3.jpg",
      *          "bank_code":"银行代码","band_account":"银行账号","cardholder":"持卡人姓名","card_type":"银行卡类型"})
+     * @Response(200, body={"code":0, "message": "","data": ""})
      */
     public function certificate (CertificateRequest $request) {
         return $this->returnJson(0, '提交成功，等待审核');
@@ -91,8 +90,9 @@ class UserController extends Controller {
 
     /**
      * 骑手实名信息
-     * @Get("/api/rider/certificate_info")
+     * @Get("/api/rider/certificateinfo")
      * @Version({"v1"})
+     * @Response(200, body={"code":0, "message": "","data": ""})
      */
     public function getCertificateInfo () {
         $data = [
@@ -109,25 +109,19 @@ class UserController extends Controller {
     }
 
     /**
-     *
-     */
-    public function getOrders () {
-
-    }
-
-    /**
-     * 骑手余额信息
+     * 骑手基本余额信息
      * @Get("/api/rider/balance")
      * @Version({"v1"})
+     * @Response(200, body={"code":0, "message": "","data": ""})
      */
-    public function balance () {
+    public function getBalance () {
         $data = [
             'balance' => 500,
             'balance_freeze' => 2,
             'details' => [
-                ['date' => '2018.02.03', 'money'=>628],
-                ['date' => '2018.02.01', 'money'=>592],
-                ['date' => '2018.01.29', 'money'=>787]
+                ['date' => '2018.02.03', 'money'=>628, 'balance' => '2500'],
+                ['date' => '2018.02.01', 'money'=>592, 'balance' => '2500'],
+                ['date' => '2018.01.29', 'money'=>787, 'balance' => '2500']
             ]
         ];
         return $this->returnJson(0, 'success', $data);
@@ -135,23 +129,43 @@ class UserController extends Controller {
 
     /**
      * 骑手余额明细
-     * @Get("/api/rider/balance_details")
+     * @Get("/api/rider/balance/details")
      * @Version({"v1"})
+     * @Response(200, body={"code":0, "message": {"type":"类型--type=1 打款 type=2 订单收入"},"data": ""})
      */
-    public function balanceDetails () {
+    public function getBalanceDetails () {
         $data = [
-
+            [
+                'month' => '2018.03',
+                'income' => 250,
+                'pay' => 15,
+                'details' => [
+                    ["datetime" => '2018.03.03 12:02:02','bank_no' => 'E1562262', 'bank_code' => '1234521212', 'money' => 25, 'type' => '1'],
+                    ["datetime" => '2018.03.03 12:02:02','bank_no' => 'E1562262', 'bank_code' => '1234521212', 'money' => 25, 'type' => '2'],
+                    ["datetime" => '2018.03.03 12:02:02','bank_no' => 'E1562262', 'bank_code' => '1234521212', 'money' => 25, 'type' => '1']
+                ]
+            ],
+            [
+                'month' => '2018.03',
+                'income' => 250,
+                'pay' => 15,
+                'details' => [
+                    ["datetime" => '2018.03.03 12:02:02','bank_no' => 'E1562262', 'bank_code' => '1234521212', 'money' => 25, 'type' => '1'],
+                    ["datetime" => '2018.03.03 12:02:02','bank_no' => 'E1562262', 'bank_code' => '1234521212', 'money' => 25, 'type' => '2'],
+                    ["datetime" => '2018.03.03 12:02:02','bank_no' => 'E1562262', 'bank_code' => '1234521212', 'money' => 25, 'type' => '1']
+                ]
+            ]
         ];
         return $this->returnJson(0, 'success', $data);
     }
 
     /**
-     * 骑手账户信息
-     * type = 1 支票 type = 2 存储
-     * @Get("/api/rider/account_info")
+     * 骑手银行账户信息
+     * @Get("/api/rider/account")
      * @Version({"v1"})
+     * @Response(200, body={"code":0, "message": "","data": ""})
      */
-    public function accountInfo () {
+    public function getAccountInfo () {
         $data = [
             'bank_code' => '124542',
             'bank_account' => '1252 12451 1545',
@@ -162,10 +176,16 @@ class UserController extends Controller {
     }
 
     /**
-     * 骑手账户设置
-     * type = 1 支票 type = 2 存储
+     * 骑手收款账户设置
      * @Post("/api/rider/account/set")
      * @Version({"v1"})
+     * @Parameters({
+     *      @Parameter("bank_code", description="银行代码", required=true),
+     *      @Parameter("account", description="银行账户", required=true),
+     *      @Parameter("cardholder", description="持卡人姓名", required=true),
+     *      @Parameter("card_type", description="银行卡类型--type = 1 支票 type = 2 存储", required=true, type="integer")
+     * })
+     * @Response(200, body={"code":0, "message": "","data": ""})
      */
     public function accountSet (AccountSetRequest $request) {
         return $this->returnJson(0, '设置成功');
@@ -173,61 +193,25 @@ class UserController extends Controller {
 
     /**
      * 骑手评价信息
-     * type = 1 满意 type =2 不满意
      * @Get("/api/rider/evaluate")
      * @Version({"v1"})
+     * @Parameters({
+     *      @Parameter("type",description="类型--type = 1 满意 type =2 不满意",default=0)
+     * })
+     * @Response(200, body={"code":0, "message": "", "data": ""})
      */
-    public function evaluate ($type = 0) {
+    public function evaluate (Request $request) {
+        $type = $request->input('type',"0");
         $data = [
             ['content' => '很好', 'type' => 1, 'date' => '2018.03.06'],
             ['content' => '差评', 'type' => 2, 'date' => '2018.04.06'],
             ['content' => '不错', 'type' => 1, 'date' => '2018.10.06']
         ];
-        if ($type !== 0) {
+        if ($type !== "0") {
             $data = array_filter($data, function ($val) use ($type) {
                 return $val['type'] == $type;
             });
         }
         return $this->returnJson(0, 'success', $data);
-    }
-
-    /**
-     * 骑手意见列表
-     * @Get("/api/rider/suggestion")
-     * @Version({"v1"})
-     */
-    public function suggestions () {
-        $data = [
-            ['content' => '来单不响', 'date' => '06.03'],
-            ['content' => '来单不响', 'date' => '06.03']
-        ];
-        return $this->returnJson(0, 'success', $data);
-    }
-
-    /**
-     * 骑手意见详情
-     * @Get("/api/rider/suggestion/$id")
-     * @Version({"v1"})
-     */
-    public function suggestion () {
-        $data = [
-            'type' => 1,
-            'content' => '这是意见',
-            'photo' => [
-                ['name' => '1.jpg'],
-                ['name' => '2.jpg']
-            ]
-        ];
-        return $this->returnJson(0, 'success', $data);
-    }
-
-    /**
-     * 骑手意见提交
-     * @Post("/api/rider/suggestion/post")
-     * @Version({"v1"})
-     * @Request({})
-     */
-    public function suggestionPost (SuggestionPostRequest $request) {
-        return $this->returnJson(0, '意见提交成功');
     }
 }
