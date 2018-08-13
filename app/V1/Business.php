bca37f9e1a5be5e4d701785955ae79010ce40185
jianru <864345280@qq.com>
2018-08-13 18:09:07 +0800

    feat: 新增商家模型
    新增商家模型

---
+++ app/V1/Business.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\V1;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Business extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    protected $table = 'business';
+
+    protected $fillable = [
+        'id','name','mobile','email'
+    ];
+    protected $hidden = [
+        'pwd','desc'
+    ];
+
+    public function getJWTIdentifier()
+  {
+      return $this->getKey();
+  }
+
+  /**
+   * Return a key value array, containing any custom claims to be added to the JWT.
+   *
+   * @return array
+   */
+  public function getJWTCustomClaims()
+  {
+      return [];
+  }
+}
